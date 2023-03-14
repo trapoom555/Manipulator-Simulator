fcc87aafbd16b3df9e08ddbfe790b1f7f9337be8
Trapoom Ukarapol <trapoom555@gmail.com>
2023-03-14 23:01:21 +0700

    refactor

--- src/client/client.ts
+++ src/client/client.ts
@@ -2,6 +2,7 @@ import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 import createFloor from './floor'
 import { Manipulator } from './manipulator'
+import { SetUIFunction } from './ui_function'
 
 // Robot
 let DHparam = [
@@ -20,130 +21,6 @@ let HneMatrix = [
     0, 0, 1, 0,
     1, 0, 0, 1
 ]
-
-function getDHParamFromTableUI() {
-    let table = document.getElementById("dh") as HTMLTableElement;
-    let lastRow = table.rows.length - 1;
-    let lastColumn = 4;
-
-    let DHparam = [];
-    for (let i = 0; i < lastRow; i++) {
-        let tmp = []
-        for (let j = 0; j < lastColumn; j++) {
-            let id = i.toString() + "," + j.toString()
-            let input = document.getElementById(id) as HTMLInputElement;
-            tmp.push(Number(input.value))
-        }
-        DHparam.push(tmp)
-    }
-    return DHparam
-}
-
-function getRhoParamFromTableUI() {
-    let table = document.getElementById("rho") as HTMLTableElement;
-    let lastRow = table.rows.length - 1;
-    let rho = []
-
-    for (let i = 0; i < lastRow; i++) {
-        let id = 'rho' + i.toString();
-        let input = document.getElementById(id) as HTMLInputElement;
-        rho.push(input.checked)
-    }
-
-    return rho
-}
-
-function getHneParamFromTableUI() {
-
-    let HneMatrix = Array<number>(16).fill(0);
-    HneMatrix[15] = 1;
-
-    for (let i = 0; i < 16; i++) {
-        if ((i+1) % 4 != 0) {
-            let id = "Hne" + i.toString();
-            console.log(id)
-            let input = document.getElementById(id) as HTMLInputElement;
-            HneMatrix[i] = Number(input.value);
-        }
-    }
-    return HneMatrix
-}
-
-function addTableRow(tableData?: number[][], isRevoluteData?: boolean[]) {
-
-    // add DH Table Row
-    let DHtable = document.getElementById("dh") as HTMLTableElement;
-    let lastRowIdx = DHtable.rows.length - 1;
-    let row = DHtable.insertRow(-1);
-    for (let i = 0; i < 4; i++) {
-        let cell = row.insertCell(i);
-        let id = lastRowIdx.toString() + "," + i.toString()
-        let data = 0;
-        if (tableData != null) {
-            data = tableData[lastRowIdx][i];
-        }
-        cell.innerHTML = "<input id='" + id + "' value=" + data.toString() + "> </input>";
-    }
-
-    // add rho Table Row
-    let rhoTable = document.getElementById("rho") as HTMLTableElement;
-    row = rhoTable.insertRow(-1);
-    let cell = row.insertCell(0);
-    if (isRevoluteData != null) {
-        if (isRevoluteData[lastRowIdx]) {
-            cell.innerHTML = "<input type='checkbox' id='rho" + lastRowIdx.toString() + "' checked> </input>";
-        }
-        else {
-            cell.innerHTML = "<input type='checkbox' id='rho" + lastRowIdx.toString() + "'> </input>";
-        }
-    }
-    else {
-        cell.innerHTML = "<input type='checkbox' id='rho" + lastRowIdx.toString() + "' checked> </input>";
-    }
-
-}
-
-function deleteTableRow() {
-    // add DH Table Row
-    let DHtable = document.getElementById("dh") as HTMLTableElement;
-    DHtable.deleteRow(DHtable.rows.length - 1);
-    // add rho Table Row
-    let rhoTable = document.getElementById("rho") as HTMLTableElement;
-    rhoTable.deleteRow(rhoTable.rows.length - 1);
-}
-
-
-let addRowBtn = document.getElementById("add-row-btn") as HTMLButtonElement;
-if (addRowBtn) {
-    addRowBtn.addEventListener("click", function () {
-        addTableRow();
-    });
-}
-
-let deleteRowBtn = document.getElementById("del-row-btn") as HTMLButtonElement;
-if (deleteRowBtn) {
-    deleteRowBtn.addEventListener("click", function () {
-        deleteTableRow();
-    });
-}
-
-let spawnRobotBtn = document.getElementById("spawn-robot") as HTMLButtonElement;
-if (spawnRobotBtn) {
-    spawnRobotBtn.addEventListener("click", function () {
-        DHparam = getDHParamFromTableUI();
-        rho = getRhoParamFromTableUI();
-        HneMatrix = getHneParamFromTableUI();
-        console.log(HneMatrix)
-        let Hne = new THREE.Matrix4().fromArray(HneMatrix)
-        m.onUpdateParam(DHparam, rho, Hne)
-    });
-}
-
-
-// Add DH-Table to UI
-for (let i = 0; i < DHparam.length; i++) {
-    addTableRow(DHparam, rho);
-}
 
 // Scene
 const scene = new THREE.Scene()
@@ -176,7 +53,6 @@ function onWindowResize() {
     camera.updateProjectionMatrix()
     renderer.setSize(window.innerWidth, window.innerHeight)
     render()
-    // scene.remove.apply(scene, scene.children);
 }
 
 function animate() {
@@ -191,6 +67,8 @@ function render() {
 
 let Hne = new THREE.Matrix4().fromArray(HneMatrix)
 let m = new Manipulator(DHparam, rho, Hne, scene)
+new SetUIFunction(m, DHparam, rho)
+
 m.draw()
 m.addGUI()
 createFloor(scene, 8)
---
+++ src/client/ui_function.ts
@@ -0,0 +1,130 @@
+import { Manipulator } from './manipulator'
+import * as THREE from 'three'
+
+class UIFunction {
+    getDHParamFromTableUI() {
+        let table = document.getElementById("dh") as HTMLTableElement;
+        let lastRow = table.rows.length - 1;
+        let lastColumn = 4;
+
+        let DHparam = [];
+        for (let i = 0; i < lastRow; i++) {
+            let tmp = []
+            for (let j = 0; j < lastColumn; j++) {
+                let id = i.toString() + "," + j.toString()
+                let input = document.getElementById(id) as HTMLInputElement;
+                tmp.push(Number(input.value))
+            }
+            DHparam.push(tmp)
+        }
+        return DHparam
+    }
+
+    getRhoParamFromTableUI() {
+        let table = document.getElementById("rho") as HTMLTableElement;
+        let lastRow = table.rows.length - 1;
+        let rho = []
+
+        for (let i = 0; i < lastRow; i++) {
+            let id = 'rho' + i.toString();
+            let input = document.getElementById(id) as HTMLInputElement;
+            rho.push(input.checked)
+        }
+
+        return rho
+    }
+
+    getHneParamFromTableUI() {
+
+        let HneMatrix = Array<number>(16).fill(0);
+        HneMatrix[15] = 1;
+
+        for (let i = 0; i < 16; i++) {
+            if ((i + 1) % 4 != 0) {
+                let id = "Hne" + i.toString();
+                console.log(id)
+                let input = document.getElementById(id) as HTMLInputElement;
+                HneMatrix[i] = Number(input.value);
+            }
+        }
+        return HneMatrix
+    }
+
+    addTableRow(tableData?: number[][], isRevoluteData?: boolean[]) {
+
+        // add DH Table Row
+        let DHtable = document.getElementById("dh") as HTMLTableElement;
+        let lastRowIdx = DHtable.rows.length - 1;
+        let row = DHtable.insertRow(-1);
+        for (let i = 0; i < 4; i++) {
+            let cell = row.insertCell(i);
+            let id = lastRowIdx.toString() + "," + i.toString()
+            let data = 0;
+            if (tableData != null) {
+                data = tableData[lastRowIdx][i];
+            }
+            cell.innerHTML = "<input id='" + id + "' value=" + data.toString() + "> </input>";
+        }
+
+        // add rho Table Row
+        let rhoTable = document.getElementById("rho") as HTMLTableElement;
+        row = rhoTable.insertRow(-1);
+        let cell = row.insertCell(0);
+        if (isRevoluteData != null) {
+            if (isRevoluteData[lastRowIdx]) {
+                cell.innerHTML = "<input type='checkbox' id='rho" + lastRowIdx.toString() + "' checked> </input>";
+            }
+            else {
+                cell.innerHTML = "<input type='checkbox' id='rho" + lastRowIdx.toString() + "'> </input>";
+            }
+        }
+        else {
+            cell.innerHTML = "<input type='checkbox' id='rho" + lastRowIdx.toString() + "' checked> </input>";
+        }
+
+    }
+
+    deleteTableRow() {
+        // add DH Table Row
+        let DHtable = document.getElementById("dh") as HTMLTableElement;
+        DHtable.deleteRow(DHtable.rows.length - 1);
+        // add rho Table Row
+        let rhoTable = document.getElementById("rho") as HTMLTableElement;
+        rhoTable.deleteRow(rhoTable.rows.length - 1);
+    }
+}
+
+export class SetUIFunction {
+    constructor(m: Manipulator, initDHparam: number[][], initRho: boolean[]) {
+        let uiFunction = new UIFunction()
+        let addRowBtn = document.getElementById("add-row-btn") as HTMLButtonElement;
+        if (addRowBtn) {
+            addRowBtn.addEventListener("click", function () {
+                uiFunction.addTableRow();
+            });
+        }
+
+        let deleteRowBtn = document.getElementById("del-row-btn") as HTMLButtonElement;
+        if (deleteRowBtn) {
+            deleteRowBtn.addEventListener("click", function () {
+                uiFunction.deleteTableRow();
+            });
+        }
+
+        let spawnRobotBtn = document.getElementById("spawn-robot") as HTMLButtonElement;
+        if (spawnRobotBtn) {
+            spawnRobotBtn.addEventListener("click", function () {
+                let DHparam = uiFunction.getDHParamFromTableUI();
+                let rho = uiFunction.getRhoParamFromTableUI();
+                let HneMatrix = uiFunction.getHneParamFromTableUI();
+                let Hne = new THREE.Matrix4().fromArray(HneMatrix)
+                m.onUpdateParam(DHparam, rho, Hne)
+            });
+        }
+
+        // Add DH-Table to UI
+        for (let i = 0; i < initDHparam.length; i++) {
+            uiFunction.addTableRow(initDHparam, initRho);
+        }
+    }
+}
